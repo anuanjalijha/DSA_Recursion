1a544ea0d613aee9a3ec543431daba5fa4d861c9
Anjali <anjalijanar@gmail.com>
2023-09-02 20:26:42 +0530

    Given a string S, remove consecutive duplicates from it recursively.

---
+++ RemoveDuplicatesRecursively.py
@@ -0,0 +1,12 @@
+# Problem ID 91, removeConsecutiveDuplicates
+def removeConsecutiveDuplicates(string):
+    if len(string)==0 or len(string)==1:
+        return string
+    if string[0]== string[1]:
+        return removeConsecutiveDuplicates(string[1:])
+    else:
+        return string[0] + removeConsecutiveDuplicates(string[1:])        
+
+# Main
+string = input().strip()
+print(removeConsecutiveDuplicates(string))
